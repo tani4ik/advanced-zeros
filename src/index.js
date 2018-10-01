6e34e3e33fe1f8488a330116ddb11c7b7d6e1d56
Tatsiana Sakalinskaya <tani4ik@gmail.com>
2018-10-01 22:45:21 +0300

    Implemented advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let z;
+  let result = number;
+  
+  for (let i = 2; i <= base; i++) {
+    if (base % i === 0) {
+      let quantityA = 0;
+      while (base % i === 0) {
+        quantityA++;
+        base /= i;
+      }
+      let quantityB = 0;
+      let n = number;
+      while (n / i > 0) {
+        quantityB += Math.floor(n / i);
+        n /= i;
+      }
+      z =  Math.floor(quantityB / quantityA); 
+      result = Math.min(result, z);
+    }
+  }
+  return result;
 }
